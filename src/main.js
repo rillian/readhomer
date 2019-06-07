c993e4a02a58046445bb2bc696b0ce8643e9135a
Ralph Giles <giles@thaumas.net>
2019-06-07 08:40:32 -0700

    Reformat global component loader.
    Make it easier for me to understand what's going on.

--- src/main.js
+++ src/main.js
@@ -7,9 +7,14 @@ import widgets from './widgets';
 
 Vue.config.productionTip = false;
 
-const files = require.context('./global-components', true, /\.vue$/i);
-files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
-  .forEach(file => Vue.component(file));
+// Register what's in the global components directory.
+const globalComponents = require.context('./global-components', true, /\.vue$/i);
+
+globalComponents.keys().forEach((path) => {
+  // Component's filename without the extension.
+  const name = path.split('/').pop().split('.')[0];
+  Vue.component(name, globalComponents(path).default);
+});
 
 Vue.use(SkeletonPlugin, { widgets });
 
